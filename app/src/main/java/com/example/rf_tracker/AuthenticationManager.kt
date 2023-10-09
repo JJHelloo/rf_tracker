845fddca06553583c48f0da8367623bd6cff42cc
JJHelloo <jjohnson099@outlook.com>
2023-10-09 16:00:52 -0700

    Add the correct route name for the authManager

--- app/src/main/java/com/example/rf_tracker/AuthenticationManager.kt
+++ app/src/main/java/com/example/rf_tracker/AuthenticationManager.kt
@@ -26,7 +26,7 @@ class AuthenticationManager(private val context: Context) {
         val client = OkHttpClient()
 
         val request = Request.Builder()
-            .url("http://10.0.2.2:3001/auth/login")
+            .url("http://10.0.2.2:3001/auth/appLogin")
             .post(body)
             .build()
 
